62b0115f6f7073895f6d49d715d686ad55f28bde
lmarin <lmarin@conectium.com>
2017-08-09 13:00:16 -0400

    creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProjectListComponent } from './auth/project-list/project-list.component';
+import { HeaderComponent } from './common/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
